e414a812949178ebeede15c0f11b16cdbdb2020b
Shalini Kumari <147317106+snpathaks@users.noreply.github.com>
2025-11-15 21:46:36 +0530

    Add files via upload

---
+++ alternateswap.cpp
@@ -0,0 +1,51 @@
+//#include<iostream>
+//using namespace std;
+//void printa(int a[],int n){
+//	       
+//	for(int i=0;i<n;i++){
+//		cout<<a[i]<<" ";
+//	}cout<<endl;
+//}
+//
+//void swapAlternate(int a[],int size){
+//	for(int i=0;i<size;i+=2){
+//		if(i+1 < size){
+//			swap(a[i],a[i+1]);
+//		}
+//	}
+//}
+//
+//
+//int main(){
+//	int e[8] ={1,34,5,6,7,8,90,67};
+//	int o[5] = {-9,23,3,10,-43};
+//	swapAlternate(e,8);
+//	printa(e,8);
+//  swapAlternate(o,5);
+//	printa(o,5);
+//	return 0;
+//}
+
+
+
+// codestudio question
+#include<iostream>
+
+
+
+//#include<iostream>
+//using namespace std
+/*
+ int findDuplicate (vector<int>, &arr) 
+ {
+   
+	int ans =0;
+	for(int i=0;i<arr.size();i++){
+		ans =ans^arr[i];
+	}
+	for(int i =1;i<arr.size();i++){
+		ans =ans^i;
+	}
+    return ans;	
+}
+*/
