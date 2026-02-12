e84256e0d8b09fdff8304517c889075fada61f21
Shalini Kumari <147317106+snpathaks@users.noreply.github.com>
2026-02-12 22:34:20 +0530

    Update ll.cpp

--- ll.cpp
+++ ll.cpp
@@ -480,3 +480,119 @@ public:
 #incldue<bits/stdc++.h>
 
 // LC : 24 Swap Nodes in Pairs--------------------------------------------------------------
+
+
+//DOUBLY LINKED LIST--------------------------------------------------------------------------------------
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node{
+//     public:
+//         int data;
+//         Node* next;
+//         Node* prev;
+//         Node(int data){
+//             this->data=data;
+//             next=NULL;
+//             prev=NULL;   
+//         }
+        
+// };
+// Node* deleteAtEnd(Node* tail){
+//      Node* temp=tail->prev;
+//     tail->prev=NULL;
+//     temp->next=NULL;
+//     return temp;
+// }
+// Node* deleteAtStart(Node* head){
+//     head->next->prev=NULL;
+//     return head->next;
+// }
+// Node* insertatEnd(Node* tail, int val){
+//     Node* nn=new Node(val);
+//     tail->next=nn;
+//     nn->prev=tail;
+//     tail=nn;
+//     return tail;
+// }
+// Node* insertAtStart(Node* head, int val){
+//     Node* newNode=new Node(val);
+//     newNode->next=head;
+//     head->prev=newNode;
+//     head=newNode;
+//     return head;
+// }
+// int main() {
+//     // Write C++ code here
+//     Node* head=new Node(5);
+//     Node* tail=head;
+//     // Insertion at start
+//     head=insertAtStart(head,4);
+//     head=insertAtStart(head,3);
+//     head=insertAtStart(head,2);
+    
+    
+//     // insertion at end
+//     tail=insertatEnd(tail,6);
+//     tail=insertatEnd(tail,7);
+//     tail=insertatEnd(tail,8);
+    
+    
+//     // deletion at start
+//     head=deleteAtStart(head);
+    
+//     // deletiion at end
+//     tail=deleteAtEnd(tail);
+    
+    
+//     // Printing the Node values
+//     Node* curr=head;
+//     while(curr){
+//         cout<<curr->data<<" ";
+//         curr=curr->next;
+//     }
+    
+    
+
+//     return 0;
+// }
+
+//Reverse a Doubly Linekd list------------------------------------------------------------------
+// /*
+// class Node {
+//   public:
+//     int data;
+//     Node *next;
+//     Node *prev;
+//     Node(int val) {
+//         data = val;
+//         next = NULL;
+//         prev = NULL;
+//     }
+// };
+
+// */
+// class Solution {
+//   public:
+//     Node *reverse(Node *head) {
+//         // code here
+//         if(!head || !head->next) return head;
+//         Node* curr = head;
+//         Node* prevn = NULL;
+//         //Node* prev = NULL;
+//         while(curr){
+//          prevn= curr->prev;
+//          curr->prev = curr->next;
+//          curr->next = prevn;
+//          curr = curr->prev;
+//         }
+//         return prevn->prev;
+         
+        
+        
+        
+        
+//     }
+// };
+
