4a5d04a5ebd2724b419bf6a988f6fa5f9598519e
Shalini Kumari <147317106+snpathaks@users.noreply.github.com>
2026-03-04 17:51:52 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -466,51 +466,8 @@ int main() {
 }*/
 
 
-//class DSA:::::::::::::::::::::::::::::
 
-#include<iostream>
-using namespace std;
-void heapify(int arr[],int n,int i){
-	int largest =i;
-	int left = 2*i +1;
-	int right = 2*i+2;
-	if(left<n && arr[left] > arr[largest]){
-		left = largest;
-	}
-	if(right<n && arr[right] > arr[largest]){
-		right = largest;
-	}
-	if(largest != 0){
-		for(int i=n/2-1;i>=0;i--){
-			swap(arr[i],arr[largest]);
-			heapify(arr,n,largest);
-		}
-	}
-}
 
-void sort(int arr[], int n){
-	for(int i=n/2-1;i>=0;i--){
-		heapify(arr,n,i);
-	}
-	for(int i=n-1;i>0;i--){
-		swap(arr[0],arr[i]);
-		heapify(arr,i,0);
-	}
-}
-void display(int arr[], int n){
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<"\n";
-	}
-}
-int main () {
-	int n;
-	cin>>n;
-	int arr[100];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-	}
-	heapify(arr,n);
-}
 
 
 
